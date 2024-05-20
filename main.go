708678fd83bb2cf6396b126ffd765f72e9354ea8
daniilkuz <d.d.kuzovlev@gmail.com>
2024-05-21 02:54:25 +0700

    added acceptLoopt and readLoop functions

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 type Server struct {
 	listenAddr string
@@ -22,12 +26,42 @@ func (s *Server) Start() error {
 	}
 	defer ln.Close()
 	s.ln = ln
+
+	go s.acceptLoop()
+
 	<-s.quitc
 	return nil
 }
 
 func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
 
+		fmt.Println("new connection to the server:", conn.RemoteAddr())
+		go s.readLoop(conn)
+	}
 }
 
-func main() {}
+func (s *Server) readLoop(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read error:", err)
+			continue
+		}
+
+		msg := buf[:n]
+		fmt.Println(string(msg))
+	}
+}
+
+func main() {
+	server := NewServer(":3000")
+	log.Fatal(server.Start())
+}
